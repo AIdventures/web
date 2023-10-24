50979eb184b634c089f53174cedcabadfe17a04e
MarioProjects <maparla@proton.me>
2023-10-24 17:22:44 +0200

    added latex

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,12 @@ import mdx from "@astrojs/mdx";
 
 import sitemap from "@astrojs/sitemap";
 
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://aidventure.es',
   integrations: [
     mdx({
       syntaxHighlight: 'shiki',
@@ -15,5 +19,8 @@ export default defineConfig({
       },
       drafts: true
     }), tailwind(), sitemap()],
-  site: 'https://aidventure.es'
+    markdown: {
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex]
+    }
 });
